a7b377783b799dbb7be24cce2691cd921810ad12
zLucky090802 <d.espitia15@gmail.com>
2026-05-21 13:17:58 -0500

    routes configured

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'home',
+        loadComponent: () => import('./pages/home-page/home-page.component').then(m => m.HomePageComponent)
+    },
+    {
+        path:'',
+        pathMatch:'full',
+        redirectTo:'home'
+    },
+    
+];
