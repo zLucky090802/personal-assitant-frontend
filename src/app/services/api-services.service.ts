4019eee95dcfa258606cd6d7e9d17d9ffa8d23db
zLucky090802 <d.espitia15@gmail.com>
2026-05-21 11:56:39 -0500

    api service, uploadFile & asquestion created

--- src/app/services/api-services.service.ts
+++ src/app/services/api-services.service.ts
@@ -1,11 +1,22 @@
-import { Injectable } from '@angular/core';
+import { Inject, inject, Injectable } from '@angular/core';
+import { enviroment } from '../../enviroments/enviroments';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ApiServicesService {
+  private base_url = enviroment.base_url;
+  private http = inject(HttpClient);
+  currentSessionId = crypto.randomUUID(); // Genera un string único como 'f47ac10b-58cc-4372-a567-0e02b2c3d479'
 
-  constructor() { }
+  uploadFIle(file: File) {
+    const formData = new FormData();
+    formData.append('file', file);
+    return this.http.post(`${this.base_url}/upload`, formData);
+  }
 
-  
+  askQuestion(sessionId: string, question: string){
+    return this.http.post(`${this.base_url}/ask`,{sessionId: sessionId,  question})
+  }
 }
